87b7139ff20198744bd5b64774f1ac285605778a
Ömer Halis DEMİR <omerhd16@outlook.com>
2026-06-13 17:42:03 +0300

    Favicon güncellenmesi yapıldı.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
  reactCompiler: true,
+ async rewrites() {
+  return [
+   {
+    source: "/favicon.ico",
+    destination: "/brand/favicon.ico",
+   },
+  ];
+ },
  images: {
   remotePatterns: [
    {
